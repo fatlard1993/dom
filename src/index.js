9f1901240fe69b79313701b2e033066ede3bce71
fatlard1993 <fatlard1993@gmail.com>
2019-07-23 16:49:03 -0700

    ditrch j-queer-y pep

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// includes js-util log pep
+// includes js-util log
 // babel
 /* global util log logHelp */
 
@@ -12,15 +12,20 @@ var dom = {
 
 		else document.addEventListener('DOMContentLoaded', dom.onLoaded);
 
-		document.addEventListener('pointerdown', dom.interact.pointerDown);
-		document.addEventListener('pointerup', dom.interact.pointerUp);
+		var n = null;
+
+		dom.interact.keyMap = [n, n, n, 'CANCEL', n, n, 'HELP', n, 'BACK_SPACE', 'TAB', n, n, 'CLEAR', 'ENTER', 'ENTER_SPECIAL', n, 'SHIFT', 'CONTROL', 'ALT', 'PAUSE', 'CAPS_LOCK', 'KANA', 'EISU', 'JUNJA', 'FINAL', 'HANJA', n, 'ESCAPE', 'CONVERT', 'NONCONVERT', 'ACCEPT', 'MODECHANGE', 'SPACE', 'PAGE_UP', 'PAGE_DOWN', 'END', 'HOME', 'LEFT', 'UP', 'RIGHT', 'DOWN', 'SELECT', 'PRINT', 'EXECUTE', 'PRINTSCREEN', 'INSERT', 'DELETE', n, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'COLON', 'SEMICOLON', 'LESS_THAN', 'EQUALS', 'GREATER_THAN', 'QUESTION_MARK', 'AT', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'OS_KEY', n, 'CONTEXT_MENU', n, 'SLEEP', 'NUMPAD0', 'NUMPAD1', 'NUMPAD2', 'NUMPAD3', 'NUMPAD4', 'NUMPAD5', 'NUMPAD6', 'NUMPAD7', 'NUMPAD8', 'NUMPAD9', 'MULTIPLY', 'ADD', 'SEPARATOR', 'SUBTRACT', 'DECIMAL', 'DIVIDE', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', n, n, n, n, n, n, n, n, 'NUM_LOCK', 'SCROLL_LOCK', 'WIN_OEM_FJ_JISHO', 'WIN_OEM_FJ_MASSHOU', 'WIN_OEM_FJ_TOUROKU', 'WIN_OEM_FJ_LOYA', 'WIN_OEM_FJ_ROYA', n, n, n, n, n, n, n, n, n, 'CIRCUMFLEX', 'EXCLAMATION', 'DOUBLE_QUOTE', 'HASH', 'DOLLAR', 'PERCENT', 'AMPERSAND', 'UNDERSCORE', 'OPEN_PAREN', 'CLOSE_PAREN', 'ASTERISK', 'PLUS', 'PIPE', 'HYPHEN_MINUS', 'OPEN_CURLY_BRACKET', 'CLOSE_CURLY_BRACKET', 'TILDE', n, n, n, n, 'VOLUME_MUTE', 'VOLUME_DOWN', 'VOLUME_UP', n, n, 'SEMICOLON', 'EQUALS', 'COMMA', 'MINUS', 'PERIOD', 'SLASH', 'BACK_QUOTE', n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, 'OPEN_BRACKET', 'BACK_SLASH', 'CLOSE_BRACKET', 'QUOTE', n, 'META', 'ALTGR', n, 'WIN_ICO_HELP', 'WIN_ICO_00', n, 'WIN_ICO_CLEAR', n, n, 'WIN_OEM_RESET', 'WIN_OEM_JUMP', 'WIN_OEM_PA1', 'WIN_OEM_PA2', 'WIN_OEM_PA3', 'WIN_OEM_WSCTRL', 'WIN_OEM_CUSEL', 'WIN_OEM_ATTN', 'WIN_OEM_FINISH', 'WIN_OEM_COPY', 'WIN_OEM_AUTO', 'WIN_OEM_ENLW', 'WIN_OEM_BACKTAB', 'ATTN', 'CRSEL', 'EXSEL', 'EREOF', 'PLAY', 'ZOOM', n, 'PA1', 'WIN_OEM_CLEAR', n];
+
+		document.addEventListener('touchstart', dom.interact.touchDown);
+		document.addEventListener('mousedown', dom.interact.mouseDown);
+		document.addEventListener('touchend', dom.interact.touchUp);
+		document.addEventListener('touchcancel', dom.interact.touchUp);
+		document.addEventListener('mouseup', dom.interact.mouseUp);
 
 		document.addEventListener('keydown', dom.interact.keyDown);
 		document.addEventListener('keyup', dom.interact.keyUp);
 
 		document.addEventListener('change', dom.interact.change);
-
-		document.oncontextmenu = dom.interact.contextMenu;
 
 		setTimeout(function acceptKeyPresses_TO(){ dom.interact.acceptKeyPresses = true; }, 1000);
 
@@ -58,28 +63,53 @@ var dom = {
 				dom.interact[eventName][x].call(dom.interact, evt);
 			}
 		},
+		mouseDown: function(evt){
+			evt.pointerType = 'mouse';
+
+			dom.interact.triggerEvent('mouseDown', evt);
+
+			dom.interact.pointerDown(evt);
+		},
+		mouseUp: function(evt){
+			evt.pointerType = 'mouse';
+
+			dom.interact.triggerEvent('mouseUp', evt);
+
+			dom.interact.pointerUp(evt);
+		},
+		touchDown: function(evt){
+			evt.pointerType = 'touch';
+
+			dom.interact.triggerEvent('touchDown', evt);
+
+			dom.interact.pointerDown(evt);
+		},
+		touchUp: function(evt){
+			evt.pointerType = 'touch';
+
+			dom.interact.triggerEvent('touchUp', evt);
+
+			dom.interact.pointerUp(evt);
+		},
 		pointerDown: function pointerDown(evt){
 			++dom.interact.activity;
 
-			if(typeof evt.target.className !== 'string') return;
+			if((dom.isMobile && evt.pointerType !== 'touch') || typeof evt.target.className !== 'string') return;
 
 			dom.interact.triggerEvent('pointerDown', evt);
 		},
 		pointerUp: function pointerUp(evt){
-			if(typeof evt.target.className !== 'string') return;
+			if((dom.isMobile && evt.pointerType !== 'touch') || typeof evt.target.className !== 'string') return;
 
 			dom.interact.triggerEvent('pointerUp', evt);
 		},
-		translateKeyCode: function translateKeyCode(keyCode){
-			var n = null, map = [n, n, n, 'CANCEL', n, n, 'HELP', n, 'BACK_SPACE', 'TAB', n, n, 'CLEAR', 'ENTER', 'ENTER_SPECIAL', n, 'SHIFT', 'CONTROL', 'ALT', 'PAUSE', 'CAPS_LOCK', 'KANA', 'EISU', 'JUNJA', 'FINAL', 'HANJA', n, 'ESCAPE', 'CONVERT', 'NONCONVERT', 'ACCEPT', 'MODECHANGE', 'SPACE', 'PAGE_UP', 'PAGE_DOWN', 'END', 'HOME', 'LEFT', 'UP', 'RIGHT', 'DOWN', 'SELECT', 'PRINT', 'EXECUTE', 'PRINTSCREEN', 'INSERT', 'DELETE', n, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'COLON', 'SEMICOLON', 'LESS_THAN', 'EQUALS', 'GREATER_THAN', 'QUESTION_MARK', 'AT', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'OS_KEY', n, 'CONTEXT_MENU', n, 'SLEEP', 'NUMPAD0', 'NUMPAD1', 'NUMPAD2', 'NUMPAD3', 'NUMPAD4', 'NUMPAD5', 'NUMPAD6', 'NUMPAD7', 'NUMPAD8', 'NUMPAD9', 'MULTIPLY', 'ADD', 'SEPARATOR', 'SUBTRACT', 'DECIMAL', 'DIVIDE', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', n, n, n, n, n, n, n, n, 'NUM_LOCK', 'SCROLL_LOCK', 'WIN_OEM_FJ_JISHO', 'WIN_OEM_FJ_MASSHOU', 'WIN_OEM_FJ_TOUROKU', 'WIN_OEM_FJ_LOYA', 'WIN_OEM_FJ_ROYA', n, n, n, n, n, n, n, n, n, 'CIRCUMFLEX', 'EXCLAMATION', 'DOUBLE_QUOTE', 'HASH', 'DOLLAR', 'PERCENT', 'AMPERSAND', 'UNDERSCORE', 'OPEN_PAREN', 'CLOSE_PAREN', 'ASTERISK', 'PLUS', 'PIPE', 'HYPHEN_MINUS', 'OPEN_CURLY_BRACKET', 'CLOSE_CURLY_BRACKET', 'TILDE', n, n, n, n, 'VOLUME_MUTE', 'VOLUME_DOWN', 'VOLUME_UP', n, n, 'SEMICOLON', 'EQUALS', 'COMMA', 'MINUS', 'PERIOD', 'SLASH', 'BACK_QUOTE', n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, 'OPEN_BRACKET', 'BACK_SLASH', 'CLOSE_BRACKET', 'QUOTE', n, 'META', 'ALTGR', n, 'WIN_ICO_HELP', 'WIN_ICO_00', n, 'WIN_ICO_CLEAR', n, n, 'WIN_OEM_RESET', 'WIN_OEM_JUMP', 'WIN_OEM_PA1', 'WIN_OEM_PA2', 'WIN_OEM_PA3', 'WIN_OEM_WSCTRL', 'WIN_OEM_CUSEL', 'WIN_OEM_ATTN', 'WIN_OEM_FINISH', 'WIN_OEM_COPY', 'WIN_OEM_AUTO', 'WIN_OEM_ENLW', 'WIN_OEM_BACKTAB', 'ATTN', 'CRSEL', 'EXSEL', 'EREOF', 'PLAY', 'ZOOM', n, 'PA1', 'WIN_OEM_CLEAR', n];
-
-			return map[keyCode];
-		},
 		keyDown: function keyDown(evt){
-			var keyPressed = dom.interact.translateKeyCode(evt.which || evt.keyCode);
+			var keyPressed = dom.interact.keyMap[evt.which || evt.keyCode];
 
 			if(dom.interact.pressedKeys[keyPressed]) return; // not yet fired keyup on this key
+
 			else if(!dom.interact.acceptKeyPresses) return (dom.interact.pressedKeys[keyPressed] = 2); // keypress while not accepting
+
 			else dom.interact.pressedKeys[keyPressed] = 1; // valid keypress
 
 			++dom.interact.activity;
@@ -91,10 +121,12 @@ var dom = {
 			dom.interact.triggerEvent('keyDown', evt);
 		},
 		keyUp: function keyUp(evt){
-			var keyPressed = dom.interact.translateKeyCode(evt.which || evt.keyCode);
+			var keyPressed = dom.interact.keyMap[evt.which || evt.keyCode];
 
 			if(!dom.interact.pressedKeys[keyPressed]) return; // keyup already fired on this key
+
 			else if(dom.interact.pressedKeys[keyPressed] === 2) return (dom.interact.pressedKeys[keyPressed] = 0); // keypress rejected due to being pressed before accepting
+
 			else dom.interact.pressedKeys[keyPressed] = 0; // valid keypress reset
 
 			dom.validate(evt.target);
@@ -107,13 +139,6 @@ var dom = {
 			dom.validate(evt.target);
 
 			dom.interact.triggerEvent('change', evt);
-		},
-		contextMenu: function contextMenu(evt){
-			if((document.activeElement && (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA'))) return;
-
-			evt.preventDefault();
-
-			dom.interact.triggerEvent('contextMenu', evt);
 		}
 	},
 	localStorage: (function initLocalStorage(){

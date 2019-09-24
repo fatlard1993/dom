46efaa5c61bb3c3839d9919a5af3745156188737
fatlard1993 <fatlard1993@gmail.com>
2019-09-24 16:27:38 -0700

    add append to label func

--- src/index.js
+++ src/index.js
@@ -303,6 +303,9 @@ var dom = {
 
 		else if(elem_s && elem_s.parentElement) elem_s.classList[add_remove](classes);
 	},
+	appendToLabel: function(elem, text, wrapper){
+		dom.createElem('label', { appendChild: elem, textContent: text, appendTo: wrapper });
+	},
 	remove: function(elem_s){
 		if(dom.isNodeList(elem_s)) elem_s = [].slice.call(elem_s);
 
